b1a10c30ba247e2cd04dcd104a31dba98efadc7b
StephenBurns <123398787+StephenBurns30303@users.noreply.github.com>
2025-01-27 12:02:18 -0500

    Groundwork
    just added the groundwork for creating a new picture by deleting the old objects and creating three new objects for each class(IE 3x Person, 3x Square, 3x Circle, and 3x Triangle).

--- Picture.java
+++ Picture.java
@@ -6,26 +6,62 @@
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author  Stephen M. Burns
+ * @version 2025.01.27
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
+ 
+
     private Triangle roof;
     private Circle sun;
     private boolean drawn;
+    
+    private Square background1;
+    
+    private Square monolith1;
+    private Square monolith2;
+    private Square monolith3;
+    
+    private Person human1;
+    private Person human2;
+    private Person human3;
+    
+    private Triangle sharp1;
+    private Triangle sharp2;
+    private Triangle sharp3;
+    
+    private Circle sun1;
+    private Circle sun2;
+    private Circle sun3;
 
     /**
      * Constructor for objects of class Picture
      */
     public Picture()
     {
-        wall = new Square();
-        window = new Square();
+        
+        background1= new Square();
+        
+        monolith1= new Square();
+        monolith2= new Square();
+        monolith3= new Square();
+        
+        human1= new Person();
+        human2= new Person();
+        human3= new Person();
+        
+        sharp1= new Triangle();
+        sharp2= new Triangle();
+        sharp3= new Triangle();
+        
+        sun1= new Circle();
+        sun2= new Circle();
+        sun3= new Circle();
+        
         roof = new Triangle();  
         sun = new Circle();
+        
         drawn = false;
     }
 
@@ -35,16 +71,19 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.makeVisible();
             
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
+            background1.moveHorizontal(-140);
+            background1.moveVertical(20);
+            background1.changeSize(120);
+            background1.makeVisible();
+            background1.changeColor("black");
+            
+            monolith1;
+            human1;
+            sharp1;
+            sun1;
+            
+
     
             roof.changeSize(60, 180);
             roof.moveHorizontal(20);
@@ -65,7 +104,7 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
+        background1.changeColor("white");
         window.changeColor("white");
         roof.changeColor("black");
         sun.changeColor("black");
@@ -76,8 +115,7 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
+        background1.changeColor("black");
         roof.changeColor("green");
         sun.changeColor("yellow");
     }
